4ed433cdbc01b2888dc33b1c1c6744085e80e429
Mr. Simi <43332042+mrsimi@users.noreply.github.com>
2022-06-24 16:03:37 +0100

    recurring email

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Hangfire;
+using Hangfire.Logging;
+using Hangfire.Logging.LogProviders;
+using Hangfire.MemoryStorage;
+using Hangfire.SqlServer;
+using Hangfire.Storage.SQLite;
+
+GlobalConfiguration.Configuration.UseSQLiteStorage("Datfaasdd409.db"); 
+RecurringJob.AddOrUpdate(() => Console.WriteLine("Hello world"), Cron.Minutely);
+
+using (new BackgroundJobServer())
+{
+    Console.WriteLine("Hangfire Server started. Press ENTER to exit...");
+    Console.ReadLine();
+}
